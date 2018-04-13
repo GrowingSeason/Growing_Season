6e9593d4b4dc83473f390dcc9b8b3504fa34cd91
정규반 <@kosmo2011>
2018-04-13 15:19:09 +0900

    Growing_Season/src/main/resources/upload.properties 	Growing_Season/target/classes/upload.properties 	Growing_Season/target/m2e-wtp/web-resources/META-INF/maven/com.kosmo/common/pom.properties

---
+++ Growing_Season/src/main/java/com/kosmo/board/BDeclarationVO.java
@@ -0,0 +1,32 @@
+package com.kosmo.board;
+
+public class BDeclarationVO {
+	private int bseq;
+	private String bdreason;
+	private int bdregdate;
+	private int bdmseq;
+	public int getBseq() {
+		return bseq;
+	}
+	public void setBseq(int bseq) {
+		this.bseq = bseq;
+	}
+	public String getBdreason() {
+		return bdreason;
+	}
+	public void setBdreason(String bdreason) {
+		this.bdreason = bdreason;
+	}
+	public int getBdregdate() {
+		return bdregdate;
+	}
+	public void setBdregdate(int bdregdate) {
+		this.bdregdate = bdregdate;
+	}
+	public int getBdmseq() {
+		return bdmseq;
+	}
+	public void setBdmseq(int bdmseq) {
+		this.bdmseq = bdmseq;
+	}
+}
---
+++ Growing_Season/src/main/java/com/kosmo/board/BoardServiceImpl.java
@@ -0,0 +1,201 @@
+package com.kosmo.board;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kosmo.mapper.BoardMapper;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired  
+	private BoardMapper boarddao;
+
+	@Override
+	public String selectMgubun(int mseq) {
+		return boarddao.selectMgubun(mseq);
+	}
+	
+	@Override
+	public Map<String,Object> boardNoticeList(int sseq, int eseq, int mseq) {
+		String mgubun=boarddao.selectMgubun(mseq);
+		ArrayList<BoardVO> list=boarddao.boardNoticeList(sseq, eseq);
+		
+		Map<String,Object> map=new HashMap<String,Object>();
+		map.put("Mgubun", mgubun);
+		map.put("nList", list);		
+		return map;
+	}
+
+	@Override
+	public ArrayList<BoardVO> boardFreeList(int sseq, int eseq) {
+		return boarddao.boardFreeList(sseq, eseq);
+	}
+
+	@Override
+	public ArrayList<BoardVO> boardProposalList(int sseq, int eseq) {
+		return boarddao.boardProposalList(sseq, eseq);
+	}
+
+	@Override
+	public int boardNoticeInsert(BoardVO vo) {
+		return boarddao.boardNoticeInsert(vo);
+	}
+
+	@Override
+	public int boardFreeInsert(BoardVO vo) {
+		return boarddao.boardFreeInsert(vo);
+	}
+
+	@Override
+	public int boardProposalInsert(BoardVO vo) {
+		return boarddao.boardProposalInsert(vo);
+	}
+
+	@Override
+	public BoardVO boardNoticeDetail(int bseq) {
+		return boarddao.boardNoticeDetail(bseq);
+	}
+	
+	@Override
+	public BoardVO boardFreeDetail(int bseq) {
+		return boarddao.boardFreeDetail(bseq);
+	}
+	
+	@Override
+	public BoardVO boardProposalDetail(int bseq) {
+		return boarddao.boardProposalDetail(bseq);
+	}
+
+	@Override
+	public int boardNoticeUpdate(BoardVO vo) {
+		boarddao.boardUpdateForUpload(vo);
+		return boarddao.boardNoticeUpdate(vo);
+	}
+
+	@Override
+	public int boardFreeUpdate(BoardVO vo) {
+		boarddao.boardUpdateForUpload(vo);
+		return boarddao.boardFreeUpdate(vo);
+	}
+	
+	@Override
+	public int boardProposalUpdate(BoardVO vo) {
+		boarddao.boardUpdateForUpload(vo);
+		return boarddao.boardProposalUpdate(vo);
+	}
+
+	@Override
+	public int boardDelete(int bseq) {
+		return boarddao.boardDelete(bseq);
+	}
+
+	@Override
+	public int bDeclarationInsert(BoardVO vo) {
+		return boarddao.bDeclarationInsert(vo);
+	}
+
+	@Override
+	public ArrayList<BDeclarationVO> bDeclarationList(int sseq, int eseq) {
+		return boarddao.bDeclarationList(sseq, eseq);
+	}
+
+	@Override
+	public int bDeclarationDelete(int bseq) {
+		return boarddao.bDeclarationDelete(bseq);
+	}
+
+	@Override
+	public Map<String,Object> replyList(int sseq, int eseq,int bseq) {
+		BoardVO fvo=boarddao.boardFreeDetail(bseq);
+		ArrayList<ReplyVO> freeRvo= boarddao.replyList(sseq, eseq, bseq);
+		
+		BoardVO pvo=boarddao.boardProposalDetail(bseq);
+		ArrayList<ReplyVO> proposalRvo= boarddao.replyList(sseq, eseq, bseq);
+		
+		Map<String,Object> map=new HashMap<String,Object>();
+		
+		map.put("freedetail", fvo);
+		map.put("proposaldetail", pvo);
+		map.put("freerlist", freeRvo);
+		map.put("proposalrlist", proposalRvo);
+		return map;
+	}
+
+	@Override
+	public int replyInsert(ReplyVO vo) {
+		return boarddao.replyInsert(vo);
+	}
+	
+	@Override
+	public ReplyVO replyDetail(int rseq) {
+		return boarddao.replyDetail(rseq);
+	}
+
+	@Override
+	public int replyUpdate(ReplyVO vo) {
+		return boarddao.replyUpdate(vo);
+	}
+
+	@Override
+	public int replyDelete(int rseq) {
+		return boarddao.replyDelete(rseq);
+	}
+
+	@Override
+	public int rDeclarationInsert(RDeclarationVO vo) {
+		return boarddao.rDeclarationInsert(vo);
+	}
+
+	@Override
+	public ArrayList<RDeclarationVO> rDeclarationList(int sseq, int eseq) {
+		return boarddao.rDeclarationList(sseq, eseq);
+	}
+
+	@Override
+	public int rDeclarationDelete(int rseq) {
+		return boarddao.rDeclarationDelete(rseq);
+	}
+
+	@Override
+	public int boardUpdateForUpload(BoardVO vo) {
+		return boarddao.boardUpdateForUpload(vo);
+	}
+
+	@Override
+	public int boardNoticeCount() {
+		return boarddao.boardNoticeCount();
+	}
+
+	@Override
+	public int boardFreeCount() {
+		return boarddao.boardFreeCount();
+	}
+
+	@Override
+	public int boardProposalCount() {
+		return boarddao.boardProposalCount();
+	}
+
+	@Override
+	public int replyCount() {
+		return boarddao.replyCount();
+	}
+
+	@Override
+	public int bdeclarationCount() {
+		return boarddao.bdeclarationCount();
+	}
+
+	@Override
+	public int rdeclarationCount() {
+		return boarddao.rdeclarationCount();
+	}
+
+	
+
+}
