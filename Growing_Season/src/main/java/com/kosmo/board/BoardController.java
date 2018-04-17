0d8dff16537f483a85b23f6fa9fb05411e6a75d1
SMW <SMW@SMW_PC>
2018-04-17 11:41:31 +0900

    mmmm

--- Growing_Season/src/main/java/com/kosmo/board/BoardController.java
+++ Growing_Season/src/main/java/com/kosmo/board/BoardController.java
@@ -3,24 +3,16 @@ package com.kosmo.board;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -87,8 +79,7 @@ public class BoardController {
 				, 5
 				, 5 
 				);
-		String  html = pu.getPagingHtml();
-
+		String  html = pu.getPagingHtml();  
 		ArrayList<BoardVO> list = service.boardFreeList(pu.getStartSeq(), pu.getEndSeq());
 
 		ModelAndView mav = new ModelAndView();
